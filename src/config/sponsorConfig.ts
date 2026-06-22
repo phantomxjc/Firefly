1f26ec4ae35b647e7dd80e6d9d8e990e40f07a43
phantomxjc <80672748+phantomxjc@users.noreply.github.com>
2026-06-22 20:15:12 +0800

    Update sponsorConfig.ts

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -39,22 +39,6 @@ export const sponsorConfig: SponsorConfig = {
 			description: "使用 微信 扫码打赏",
 			enabled: true,
 		},
-		{
-			name: "ko-fi",
-			icon: "simple-icons:kofi",
-			qrCode: "",
-			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: true,
-		},
-		{
-			name: "爱发电",
-			icon: "simple-icons:afdian",
-			qrCode: "",
-			link: "https://ifdian.net/a/cuteleaf",
-			description: "通过 爱发电 进行打赏",
-			enabled: true,
-		},
 	],
 
 	// 打赏者列表（可选）
